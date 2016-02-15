eb6f09e0ad9f882af025af6e18c17bd951691cbe
Bilal Qayyum <bilal.qayyum@tintash.com>
2016-02-16 00:15:30 +0500

    Added utf-8 charset

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -3,10 +3,10 @@ class ScraperController < ApplicationController
   end
 
   def scrape
-    web_page = Scraper.new(params[:url])
-    web_page.highlight(params[:tag]) if params[:tag].present?
+    @web_page = Scraper.new(params[:url])
+    @web_page.highlight(params[:tag]) if params[:tag].present?
 
-    @page_source = web_page.page_source
-    @tags_count = web_page.tags_count
+    @page_source = @web_page.page_source
+    @tags_count = @web_page.tags_count
   end
 end
